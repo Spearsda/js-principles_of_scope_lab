5dc6ca00c11b2c4ae59354820a753610e8e0b9af
Damian <spearsda16@gmail.com>
2021-04-03 20:10:58 -0400

    donw with assingment

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Write your solution in this file!
+
+var customerName = 'bob'
+
+function upperCaseCustomerName() {
+
+    customerName = customerName.toUpperCase()
+
+    return customerName
+}
+
+
+function setBestCustomer() {
+
+    bestCustomer = 'not bob'
+
+    return bestCustomer
+}
+
+function overwriteBestCustomer() {
+
+    bestCustomer = 'maybe bob'
+}
+
+
+function changeLeastFavoriteCustomer() {
+
+    const leastFavoriteCustomer = 'Alvee'
+
+    leastFavoriteCustomer = 'Damian'
+}
